a49ab03d531de33327d8124b8b6ee7ba242cd21a
McKay Marston <mckay.marston@greymanlabs.com>
2013-05-19 18:20:35 -0600

    Added a 'WriteToFile' function
    This is a wrapper around the []byte casting. Not sure if this is the
    most idiomatically Golang way to do it (I doubt it, actually), but it
    will do for now.

--- GetFARMSEpub.go
+++ GetFARMSEpub.go
@@ -46,6 +46,13 @@ type Chapter struct {
 	Text  string
 }
 
+func WriteToFile(file io.Writer, str string) {
+	_, err := file.Write([]byte(str))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func BookData(resp *http.Response) (string, string, string) {
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(resp.Body)
@@ -104,7 +111,7 @@ func AddMimetype(zippy *zip.Writer) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	mimetype.Write([]byte("application/epub+zip"))
+	WriteToFile(mimetype, "application/epub+zip")
 }
 
 func AddContainer(zippy *zip.Writer) {
@@ -112,7 +119,7 @@ func AddContainer(zippy *zip.Writer) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	container.Write([]byte("<?xml version=\"1.0\"?><container version=\"1.0\" xmlns=\"urn:oasis:names:tc:opendocument:xmlns:container\"><rootfiles><rootfile full-path=\"OEBPS/content.opf\" media-type=\"application/oebps-package+xml\"/></rootfiles></container>"))
+	WriteToFile(container, "<?xml version=\"1.0\"?><container version=\"1.0\" xmlns=\"urn:oasis:names:tc:opendocument:xmlns:container\"><rootfiles><rootfile full-path=\"OEBPS/content.opf\" media-type=\"application/oebps-package+xml\"/></rootfiles></container>")
 }
 
 func AddHeader(title string, author string, url string, chapters []Chapter, zippy *zip.Writer) {
@@ -121,13 +128,13 @@ func AddHeader(title string, author string, url string, chapters []Chapter, zipp
 		log.Fatal(err)
 	}
 
-	content.Write([]byte("<?xml version=\"1.0\"?><package version=\"2.0\" xmlns=\"http://www.idpf.org/2007/opf\" unique-identifier=\"BookId\"><metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:opf=\"http://www.idpf.org/2007/opf\"><dc:title>"))
-	content.Write([]byte(title))
-	content.Write([]byte("</dc:title><dc:creator opf:role=\"aut\">"))
-	content.Write([]byte(author))
-	content.Write([]byte("</dc:creator><dc:language>en-US</dc:language><dc:identifier id=\"BookId\">urn:uuid:"))
-	content.Write([]byte(url))
-	content.Write([]byte("</dc:identifier></metadata>"))
+	WriteToFile(content, "<?xml version=\"1.0\"?><package version=\"2.0\" xmlns=\"http://www.idpf.org/2007/opf\" unique-identifier=\"BookId\"><metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:opf=\"http://www.idpf.org/2007/opf\"><dc:title>")
+	WriteToFile(content, title)
+	WriteToFile(content, "</dc:title><dc:creator opf:role=\"aut\">")
+	WriteToFile(content, author)
+	WriteToFile(content, "</dc:creator><dc:language>en-US</dc:language><dc:identifier id=\"BookId\">urn:uuid:")
+	WriteToFile(content, url)
+	WriteToFile(content, "</dc:identifier></metadata>")
 
 	AddContentHeader(content, chapters)
 }
@@ -136,19 +143,19 @@ func AddContentHeader(header io.Writer, chapters []Chapter) {
 	toc_pre_str := "<item id=\"chapter%d\" href=\"chapter%d.xhtml\" media-type=\"application/xhtml+xml\"/>"
 	toc_post_str := "<itemref idref=\"chapter%d\"/>"
 
-	header.Write([]byte("<manifest><item id=\"ncx\" href=\"toc.ncx\" media-type=\"text/xml\" /><item id=\"title\" href=\"title.xhtml\" media-type=\"application/xhtml+xml\"/>"))
+	WriteToFile(header, "<manifest><item id=\"ncx\" href=\"toc.ncx\" media-type=\"text/xml\" /><item id=\"title\" href=\"title.xhtml\" media-type=\"application/xhtml+xml\"/>")
 
 	for chapter := range chapters {
-		header.Write([]byte(fmt.Sprintf(toc_pre_str, chapter, chapter)))
+		WriteToFile(header, fmt.Sprintf(toc_pre_str, chapter, chapter))
 	}
 
-	header.Write([]byte("</manifest><spine toc=\"ncx\"><itemref idref=\"title\"/>"))
+	WriteToFile(header, "</manifest><spine toc=\"ncx\"><itemref idref=\"title\"/>")
 
 	for chapter := range chapters {
-		header.Write([]byte(fmt.Sprintf(toc_post_str, chapter)))
+		WriteToFile(header, fmt.Sprintf(toc_post_str, chapter))
 	}
 
-	header.Write([]byte("</spine></package>"))
+	WriteToFile(header, "</spine></package>")
 }
 
 func AddTOC(title string, book_str string, chapters []Chapter, zippy *zip.Writer) {
@@ -157,13 +164,13 @@ func AddTOC(title string, book_str string, chapters []Chapter, zippy *zip.Writer
 		log.Fatal(err)
 	}
 
-	toc.Write([]byte(fmt.Sprintf("<?xml version=\"1.0\" encoding=\"UTF-8\"?><ncx xmlns=\"http://www.daisy.org/z3986/2005/ncx/\" version=\"2005-1\"><head><meta name=\"dtb:uid\" content=\"%s\"/><meta name=\"dtb:depth\" content=\"1\"/><meta name=\"dtb:totalPageCount\" content=\"0\"/><meta name=\"dtb:maxPageNumber\" content=\"0\"/></head><docTitle><text>%s</text></docTitle><navMap><navPoint id=\"title\" playOrder=\"1\"><navLabel><text>Title Page</text></navLabel><content src=\"title.xhtml\"/></navPoint>", book_str, title)))
+	WriteToFile(toc, fmt.Sprintf("<?xml version=\"1.0\" encoding=\"UTF-8\"?><ncx xmlns=\"http://www.daisy.org/z3986/2005/ncx/\" version=\"2005-1\"><head><meta name=\"dtb:uid\" content=\"%s\"/><meta name=\"dtb:depth\" content=\"1\"/><meta name=\"dtb:totalPageCount\" content=\"0\"/><meta name=\"dtb:maxPageNumber\" content=\"0\"/></head><docTitle><text>%s</text></docTitle><navMap><navPoint id=\"title\" playOrder=\"1\"><navLabel><text>Title Page</text></navLabel><content src=\"title.xhtml\"/></navPoint>", book_str, title))
 
 	for chapter := range chapters {
-		toc.Write([]byte(fmt.Sprintf("<navPoint id=\"chapter%d\" playOrder=\"%d\"><navLabel><text>%s</text></navLabel><content src=\"chapter%d.xhtml\"/></navPoint>", chapter, chapter+2, chapters[chapter].Title, chapter)))
+		WriteToFile(toc, fmt.Sprintf("<navPoint id=\"chapter%d\" playOrder=\"%d\"><navLabel><text>%s</text></navLabel><content src=\"chapter%d.xhtml\"/></navPoint>", chapter, chapter+2, chapters[chapter].Title, chapter))
 	}
 
-	toc.Write([]byte("</navMap></ncx>"))
+	WriteToFile(toc, "</navMap></ncx>")
 }
 
 func AddTitle(book_title string, book_author string, zippy *zip.Writer) {
@@ -172,7 +179,7 @@ func AddTitle(book_title string, book_author string, zippy *zip.Writer) {
 		log.Fatal(err)
 	}
 
-	title.Write([]byte(fmt.Sprintf("<html>\n\t<head>\n\t\t<title>%s</title>\n\t</head>\n\t<body>\n\t\t<center><h1>%s</h1>\n\t\t<h2>by %s</h2></center>\n\t</body>\n</html>", book_title, book_title, book_author)))
+	WriteToFile(title, fmt.Sprintf("<html>\n\t<head>\n\t\t<title>%s</title>\n\t</head>\n\t<body>\n\t\t<center><h1>%s</h1>\n\t\t<h2>by %s</h2></center>\n\t</body>\n</html>", book_title, book_title, book_author))
 }
 
 func AddChapters(chapters []Chapter, zippy *zip.Writer) {
@@ -185,7 +192,7 @@ func AddChapters(chapters []Chapter, zippy *zip.Writer) {
 			log.Fatal(err)
 		}
 
-		chapter_file.Write([]byte(fmt.Sprintf(chapter_fmt, chapters[chapter].Title, chapters[chapter].Title, chapters[chapter].Text)))
+		WriteToFile(chapter_file, fmt.Sprintf(chapter_fmt, chapters[chapter].Title, chapters[chapter].Title, chapters[chapter].Text))
 	}
 }
 
